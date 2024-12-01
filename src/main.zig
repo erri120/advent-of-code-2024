c3699384844e4f9b404ce35dab2806f60d9677a5
erri120 <erri120@fossmailer.de>
2024-12-01 14:41:38 +0100

    Day 1: Part 2

--- src/main.zig
+++ src/main.zig
@@ -8,13 +8,13 @@ pub fn main() !void {
     var inputs_dir = try std.fs.cwd().openDir("inputs", .{});
     defer inputs_dir.close();
 
-    var file = try get_file(inputs_dir, 1, 1, false);
+    var file = try get_file(inputs_dir, 1, 2, false);
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     const reader = buf_reader.reader();
 
-    const result = try day1_part1(reader.any(), allocator);
+    const result = try day1_part2(reader.any(), allocator);
     std.debug.print("result={}", .{result});
 }
 
@@ -26,7 +26,6 @@ fn get_file(inputs_dir: std.fs.Dir, comptime day: u8, comptime part: u8, comptim
     return file;
 }
 
-const queue = std.PriorityDequeue(u64, void, lessThan);
 fn lessThan(context: void, a: u64, b: u64) std.math.Order {
     _ = context;
     return std.math.order(a, b);
@@ -49,6 +48,7 @@ fn smallest(comptime T: type, a: T, b: T) T {
 }
 
 fn day1_part1(reader: std.io.AnyReader, allocator: std.mem.Allocator) !u64 {
+    const queue = std.PriorityDequeue(u64, void, lessThan);
     var left_numbers = queue.init(allocator, {});
     defer left_numbers.deinit();
 
@@ -83,6 +83,53 @@ fn day1_part1(reader: std.io.AnyReader, allocator: std.mem.Allocator) !u64 {
     return total_distance;
 }
 
+fn day1_part2(reader: std.io.AnyReader, allocator: std.mem.Allocator) !u64 {
+    var left_counts = std.AutoHashMap(u64, u64).init(allocator);
+    defer left_counts.deinit();
+
+    var right_counts = std.AutoHashMap(u64, u64).init(allocator);
+    defer right_counts.deinit();
+
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var split = std.mem.split(u8, line, " ");
+        const left_number = try std.fmt.parseInt(u64, split.first(), 10);
+
+        var right_number: u64 = 0;
+        while (split.next()) |part| {
+            if (part.len == 0) {
+                continue;
+            }
+
+            right_number = try std.fmt.parseInt(u64, part, 10);
+        }
+
+        const left_entry = try left_counts.getOrPut(left_number);
+        if (!left_entry.found_existing) {
+            left_entry.value_ptr.* = 1;
+        } else {
+            left_entry.value_ptr.* += 1;
+        }
+
+        const right_entry = try right_counts.getOrPut(right_number);
+        if (!right_entry.found_existing) {
+            right_entry.value_ptr.* = 1;
+        } else {
+            right_entry.value_ptr.* += 1;
+        }
+    }
+
+    var similarity_score: u64 = 0;
+    var iterator = left_counts.iterator();
+    while (iterator.next()) |left_kv| {
+        const left_number = left_kv.key_ptr.*;
+        const right_count = right_counts.get(left_number) orelse 0;
+        similarity_score += left_number * right_count * left_kv.value_ptr.*;
+    }
+
+    return similarity_score;
+}
+
 test "day1 part1 example" {
     const allocator = std.testing.allocator;
 
@@ -94,4 +141,17 @@ test "day1 part1 example" {
 
     const result = try day1_part1(file.reader().any(), allocator);
     try std.testing.expectEqual(11, result);
+}
+
+test "day1 part2 example" {
+    const allocator = std.testing.allocator;
+
+    var inputs_dir = try std.fs.cwd().openDir("inputs", .{});
+    defer inputs_dir.close();
+
+    var file = try get_file(inputs_dir, 1, 2, true);
+    defer file.close();
+
+    const result = try day1_part2(file.reader().any(), allocator);
+    try std.testing.expectEqual(31, result);
 }
